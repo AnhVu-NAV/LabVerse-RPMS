277e33e93ed9cc4f188dbe84f8c3263035212385
huyendieu8304 <huyendieu8304@gmail.com>
2025-10-20 02:00:59 +0700

    Add PaperAnnotation.java entity, PaperAnnotationDao.java and modify AppDatabase.java

--- app/src/main/java/com/prm392/g5/labverse/config/AppDatabase.java
+++ app/src/main/java/com/prm392/g5/labverse/config/AppDatabase.java
@@ -7,20 +7,23 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
+import com.prm392.g5.labverse.dao.PaperAnnotationDao;
 import com.prm392.g5.labverse.dao.PaperDao;
 import com.prm392.g5.labverse.entity.Paper;
+import com.prm392.g5.labverse.entity.PaperAnnotation;
 import com.prm392.g5.labverse.util.LocalDateTimeConverter;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Paper.class}, version = 1, exportSchema = false)
+@Database(entities = {Paper.class, PaperAnnotation.class}, version = 1, exportSchema = false)
 @TypeConverters({LocalDateTimeConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
 
     private final static String DATABASE_NAME = "LabVerse.db";
 
     public abstract PaperDao paperDao();
+    public abstract PaperAnnotationDao paperAnnotationDao();
 
     // Thread pool cho tác vụ ghi DB
     public static final ExecutorService databaseWriteExecutor =
---
+++ app/src/main/java/com/prm392/g5/labverse/entity/PaperAnnotation.java
@@ -0,0 +1,84 @@
+package com.prm392.g5.labverse.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity(
+        tableName = "paper_annotation",
+        indices = {@Index(value = {"user_id", "paper_id"}, unique = true)}
+)
+public class PaperAnnotation {
+    @PrimaryKey
+    @NonNull
+    public String id;
+
+    @ColumnInfo(name = "user_id")
+    public String userId;
+
+    @ColumnInfo(name = "paper_id")
+    public String paperId;
+
+    private String annotationS3Key;
+
+    @ColumnInfo(name = "updated_at")
+    public String updatedAt; // ISO 8601 string 2025-10-17T08:05:23Z
+
+
+    public PaperAnnotation() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(String paperId) {
+        this.paperId = paperId;
+    }
+
+
+    public String getAnnotationS3Key() {
+        return annotationS3Key;
+    }
+
+    public void setAnnotationS3Key(String annotationS3Key) {
+        this.annotationS3Key = annotationS3Key;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt() {
+        this.updatedAt = Instant.now().toString();
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
