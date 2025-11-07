fb195fb9482d34515ec6b37a64e4ff0b7e157158
huyendieu8304 <huyendieu8304@gmail.com>
2025-11-07 12:54:22 +0700

    modify login (user role)

--- app/src/main/java/com/prm392/g5/labverse/dto/auth/LoginResponse.java
+++ app/src/main/java/com/prm392/g5/labverse/dto/auth/LoginResponse.java
@@ -3,10 +3,12 @@ package com.prm392.g5.labverse.dto.auth;
 public class LoginResponse{
         private String accessToken;
         private String userId;
+        private String userRole;
 
-    public LoginResponse(String accessToken, String userId) {
+    public LoginResponse(String accessToken, String userId, String userRole) {
         this.accessToken = accessToken;
         this.userId = userId;
+        this.userRole = userRole;
     }
 
     public String getAccessToken() {
@@ -15,5 +17,9 @@ public class LoginResponse{
 
     public String getUserId() {
         return userId;
+    }
+
+    public String getUserRole() {
+        return userRole;
     }
 }
